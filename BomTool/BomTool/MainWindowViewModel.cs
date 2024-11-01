5430e924c793db782d36b3b76555c82cb8508965
liumenghui <liumenghui@sofarsolar.com>
2024-11-01 16:45:35 +0800

    增加基础

---
+++ BomTool/BomTool/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BomTool;
+
+public partial class MainWindowViewModel : ObservableRecipient
+{
+    [ObservableProperty]
+    private int selectedIndex;
+
+    [ObservableProperty]
+    private bool _isConnected;
+
+    [ObservableProperty]
+    private string? _connStatus;
+
+    [ObservableProperty]
+    private int _sendBytesCount;
+
+    [ObservableProperty]
+    private int _receiveBytesCount;
+
+    [ObservableProperty]
+    private int _txCount;
+
+    [ObservableProperty]
+    private int _rxCount;
+
+    [ObservableProperty]
+    private string? _lossPercent;
+
+    public MainWindowViewModel()
+    {
+    }
+
+
+    [RelayCommand]
+    private void Loaded()
+    {
+        
+    }
+}
---
+++ BomTool/BomTool/Models/CoordinateModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BomTool.Models
+{
+    internal class CoordinateModel
+    {
+        public string? PartType { get; set; }
+        public string? RefDes { get; set; }
+        public string? PartDecal { get; set; }
+        public string? Pins { get; set; }
+        public string? Layer { get; set; }
+        public string? Orient { get; set; }
+        public string? X { get; set; }
+        public string? Y { get; set; }
+        public string? SMD { get; set; }
+        public string? Glued { get; set; }
+
+    }
+}
