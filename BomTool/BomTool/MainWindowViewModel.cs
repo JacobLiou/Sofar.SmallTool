77e039ad93385509ab55db7c9e9a0125a00dac24
liumenghui <liumenghui@sofarsolar.com>
2024-11-04 11:23:21 +0800

    增加板文件布局

--- BomTool/BomTool/MainWindowViewModel.cs
+++ BomTool/BomTool/MainWindowViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
 
 namespace BomTool;
 
 public partial class MainWindowViewModel : ObservableRecipient
 {
+    [ObservableProperty]
+    private string _droppedText;
+
+    [ObservableProperty]
+    private bool _isDraggingOver;
+
     [ObservableProperty]
     private int selectedIndex;
 
@@ -33,10 +42,59 @@ public partial class MainWindowViewModel : ObservableRecipient
     {
     }
 
-
     [RelayCommand]
     private void Loaded()
     {
-        
+
     }
+
+    [RelayCommand]
+    private void OnDragEnter(DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop) ||
+            e.Data.GetDataPresent(DataFormats.Text))
+        {
+            e.Effects = DragDropEffects.Copy;
+            IsDraggingOver = true;
+        }
+        else
+        {
+            e.Effects = DragDropEffects.None;
+            IsDraggingOver = false;
+        }
+        e.Handled = true;
+    }
+
+    [RelayCommand]
+    private async void OnDrop(DragEventArgs e)
+    {
+        IsDraggingOver = false;
+
+        try
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files?.Length > 0)
+                {
+                    // 读取第一个文件的内容
+                    DroppedText = await File.ReadAllTextAsync(files[0]);
+                }
+            }
+            else if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                DroppedText = (string)e.Data.GetData(DataFormats.Text);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"处理拖放数据时出错：{ex.Message}");
+        }
+
+        e.Handled = true;
+    }
+
+
+
+
 }
