aec161d82a44c4b491aedfb2b198ac53c571c348
liumenghui <liumenghui@sofarsolar.com>
2024-11-05 11:22:26 +0800

    增加拖拽效果

---
+++ BomTool/BomTool/Behaviors/DragAcceptDescription.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace BomTool.Behaviors
+{
+    public sealed class DragAcceptDescription
+    {
+        public event Action<DragEventArgs> DragOver;
+
+        public void OnDragOver(DragEventArgs dragEventArgs)
+        {
+            var handler = DragOver;
+            if (handler != null)
+            {
+                handler(dragEventArgs);
+            }
+        }
+
+        public event Action<DragEventArgs> DragDrop;
+
+        public void OnDrop(DragEventArgs dragEventArgs)
+        {
+            var handler = DragDrop;
+            if (handler != null)
+            {
+                handler(dragEventArgs);
+            }
+        }
+    }
+}
